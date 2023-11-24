11d52a785214d7cf16cddc0997b31221afff6795
NaazMN <149914907+NaazMN@users.noreply.github.com>
2023-11-24 15:36:52 +0530

    Delete python/first name.py

--- python/first name.py
+++
@@ -1,7 +0,0 @@
-name=input("enter first names:\n")
-namelist=name.split(',')
-count=0
-for i in namelist:
-    if i[0]=='a' or i[0]=='A':
-        count=count+1
-print("the number of names that stands with a is ",count)
